4325ed1d281129f715d508cf48fa5dead1cb07db
Geoff Appleby <geoff@gapple.ca>
2018-03-31 16:18:57 -0700

    First Slides

---
+++ js/reveal-print.js
@@ -0,0 +1,8 @@
+'use strict';
+(function () {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = window.location.search.match(/print-pdf/gi) ? 'css/print/pdf.css' : 'css/print/paper.css';
+  document.getElementsByTagName('head')[0].appendChild(link);
+})();
